aa597cdd4a7af57ef69474c01147efc212d83373
rahulremanan <rahul@remanan.net>
2018-07-17 23:57:46 -0400

    Added installer for dependencies for project RattleSnake

---
+++ installation/Install_dependencies.sh
@@ -0,0 +1,5 @@
+wget https://launchpad.net/veracrypt/trunk/1.22/+download/veracrypt-1.22-setup.tar.bz2
+mkdir ./tmp_veracrypt/
+tar xjvf veracrypt-1.22-setup.tar.bz2 -C ./tmp_veracrypt
+./tmp_veracrypt/veracrypt-1.22-setup-console-x64
+rm -r ./tmp_veracrypt/
