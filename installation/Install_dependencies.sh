6bb7ddf3d5cb284c268367787bc2a69025972316
rahulremanan <rahul@remanan.net>
2018-07-19 15:14:44 -0400

    Revised installer for dependencies, uploaded Jupyter notebook reference implementation

--- installation/Install_dependencies.sh
+++ installation/Install_dependencies.sh
@@ -1,3 +1,4 @@
+apt-get install -y ntfs-3g
 wget https://launchpad.net/veracrypt/trunk/1.22/+download/veracrypt-1.22-setup.tar.bz2
 mkdir ./tmp_veracrypt/
 tar xjvf veracrypt-1.22-setup.tar.bz2 -C ./tmp_veracrypt
